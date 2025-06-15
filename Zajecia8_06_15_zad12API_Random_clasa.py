94b07d8ff3c79b957d15b87aff5d09883e1ad3c5
Przemysław Górski <przemyslaw.gorski@gmail.com>
2025-06-15 15:57:06 +0200

    Zajecia nr 8 API Random  BaseModel wypisanie danych przypadkowego uzytkownika na ekran

--- Zajecia8_06_15_zad12API_Random_clasa.py
+++ Zajecia8_06_15_zad12API_Random_clasa.py
@@ -27,6 +27,17 @@ class User(BaseModel):
 user_data = User(**data)
 
 print(user_data)
+print(50*'=')
+
+print(f"Imię: {user_data.name.first}")
+print(f"Nazwisko: {user_data.name.last}")
+print(f"Tytuł: {user_data.name.title}")
+print(f"Telefon: {user_data.phone}")
+print(f"E-mail: {user_data.email}")
+print(f"Zdjęcie (duży format): {user_data.picture.large}")
+print(f"Zdjęcie (średni format): {user_data.picture.medium}")
+print(f"Zdjęcie (miniatura): {user_data.picture.thumbnail}")
+print(50*'=')
 
 # ustawianie pliku z danych
 file_name = f"{user_data.name.first}_{user_data.name.last}.jpg"
@@ -34,6 +45,7 @@ print(f"Nazwa pliku: {file_name}")
 
 # Pobranie zdjęcia
 response_photo = requests.get(user_data.picture.large)
+print(50*'=')
 
 # Zapisanie zdjęcia do pliku
 with open(file_name, "wb") as f:
